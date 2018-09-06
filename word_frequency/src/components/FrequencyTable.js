7d040846d07b368c1c4342cb5c52fd9a7da41ef1
StuartCharters <charters.stuart@gmail.com>
2018-09-06 20:39:12 +0100

    can get word frequencies with punctuation attached

--- word_frequency/src/components/FrequencyTable.js
+++ word_frequency/src/components/FrequencyTable.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 const FrequencyTable = (props) => {
+
+  const words = getWordFrequencies(props.phrases)
+  console.log(words);
+
   return (
-    <p>I am the freq table</p>
+    <table id="freq-table">
+      <tbody>
+
+      </tbody>
+    </table>
   );
 };
 
 export default FrequencyTable;
+
+function getWordFrequencies(phrases) {
+  return phrases.reduce((words, phrase) => {
+    console.log(phrase);
+    const phraseArray = phrase.split(' ')
+    phraseArray.forEach((word) => {
+      if (words[word]){
+        words[word] += 1
+      }else{
+        words[word] = 1
+      }
+    });
+    return words;
+  }, {});
+}
--- word_frequency/src/tests/FrequencyTable.test.js
+++ word_frequency/src/tests/FrequencyTable.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FrequencyTable from '../components/FrequencyTable.js';
+import Analyser from '../containers/Analyser.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
