de1a98f83fe36dda402f6df73c76c8376ff6c50f
StuartCharters <charters.stuart@gmail.com>
2018-09-06 18:35:17 +0100

    set up file structure of containers and components

--- word_frequency/src/App.js
+++ word_frequency/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Analyser from './containers/Analyser.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Analyser/>
       </div>
     );
   }
